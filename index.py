b0114adeba79870b4f80f2dbfe136bd404feadcd
xtabich <46863906+xtabich@users.noreply.github.com>
2019-01-23 17:44:19 +0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,3 +1,11 @@
-a = 5
-b = 4
-print (a+b)
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('746437123:AAEdzEqrsFNuegQPrMFfwEEz23ZLTouGRLQ')
+
+@bot.message_handler(commands=["start"])
+def start(message):
+	bot.send_message(message.chat.id, 'Hello world')
+	
+if __name__ == '__main__':
+    bot.polling()
